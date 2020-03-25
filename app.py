d6544ede542372da5c89dabe2384093ff20a277c
Nikita Ardashev <nikitaardashev2004@yandex.ru>
2020-03-26 00:21:31 +0500

    Clean up

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
+
 @app.route('/')
 @app.route('/index')
+def index():
+	return render_template('index.html', page_title='main')
 
-def main_page():
+@app.route('/user/<int:user_id>')
+def user(user_id):
+	# TODO get user data by user_id
+	return render_template('user.html', user=None  # User data
+	)
 
-	render_template('index.html', page_title='main')
+@app.route('/track/<int:track_id>')
+def track(track_id):
+	# TODO get track data by track_id
+	return render_template('track.html', track_id=None  # Track data
+	)
 
 if __name__ == '__main__':
-
-	app.run(port=8080, host='127.0.0.1')
+	app.run()
